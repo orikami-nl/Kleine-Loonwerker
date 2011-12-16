c65be89558686334ad6f585b2c344c355eadfc35
Thijs van de Laar <thijsvdlaar@hotmail.com>
2011-12-16 15:33:45 +0100

    submenu

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -3,6 +3,7 @@ class ContactController < ApplicationController
   end
 
   def send_contact
+		redirect_to root_path, :notice => "Uw bericht is verzonden, u hoort snel iets van ons terug."
   end
 
 end
