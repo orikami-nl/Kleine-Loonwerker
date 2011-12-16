0dd31cac548bffb4dd41b7294d57345ec4860a39
Thijs van de Laar <thijsvdlaar@hotmail.com>
2011-12-16 16:49:37 +0100

    honeypot

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -1,9 +1,34 @@
 class ContactController < ApplicationController
+	before_filter :check_honeypots, :only => [:send_contact]
+
+  def check_honeypots
+    return true if honeypot_untouched?
+    flash[:notice] = 'U bent een spambot, of gebruikt een vreemd script. (Zo niet, dan heb ik overdreven gereageerd. Probeer het alstublieft nog eens.)'
+    redirect_to :controller => "contact", :action => "index", :contact_mail => params[:contact_mail]
+    return false
+  end
+
+  def honeypot_untouched?
+    submitted = params['its_so_sweet']
+    return false if submitted.blank?
+    submitted['email'] == 'john@doe.com' && submitted['name'] == '' && submitted['agree'].blank?
+	end
+
   def index
+    @contact_mail = ContactMail.new(params[:contact_mail])
   end
 
   def send_contact
-		redirect_to root_path, :notice => "Uw bericht is verzonden, u hoort snel iets van ons terug."
+    @contact_mail = ContactMail.new(params[:contact_mail])
+		if @contact_mail.deliver
+      respond_to do |format|
+        format.html { redirect_to root_path, :notice => "Uw bericht is verzonden, u hoort snel iets van ons terug."
+}
+        format.js {render}
+			end
+    else
+      render :action => :index
+    end
   end
 
 end
