33ef74632aa2fab2693d790006efa541320ded21
Thijs van de Laar <thijsvdlaar@hotmail.com>
2011-12-16 16:11:57 +0100

    navigation active

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+	def controller_active?(controller)
+		if request.path_parameters[:controller] == controller
+			true
+		else
+			false
+		end
+	end
 end
--- app/helpers/diensten_helper.rb
+++ app/helpers/diensten_helper.rb
@@ -1,2 +1,9 @@
 module DienstenHelper
+	def action_active?(action)
+		if request.path_parameters[:action] == action
+			true
+		else
+			false
+		end
+	end
 end
