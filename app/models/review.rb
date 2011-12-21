010aa6af1874af29b7a350837d818ac166c70eaa
Thijs van de Laar <thijsvdlaar@hotmail.com>
2011-12-21 15:23:14 +0100

    referenties admin gedeelte

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,14 +2,14 @@ class Review < ActiveRecord::Base
 	attr_accessible :name, :comment, :city, :show_on_frontpage, :rating
 	validates_numericality_of :rating, :only_integer => true
 	validates_inclusion_of :rating, :in => 1..5
+	validate :no_more_than_three_reviews_on_frontpage
 
-	before_save :check_show_on_frontpage
   after_initialize :default_values
 
-	def check_show_on_frontpage
+	def no_more_than_three_reviews_on_frontpage
 		shown_reviews = Review.where(:show_on_frontpage => true)
-		if shown_reviews.count > 3
-			self.show_on_frontpage = false
+		if self.show_on_frontpage == true && shown_reviews.count >= 3
+	     errors.add(:show_on_frontpage, "Je kunt niet meer dat drie referenties op de voorpagina zetten")
 		end
 	end
 
