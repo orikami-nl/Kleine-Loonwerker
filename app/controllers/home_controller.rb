df69d80b99378c0976e28b4b2c6e2f5015d486e1
Thijs van de Laar <thijsvdlaar@hotmail.com>
2011-12-22 15:00:56 +0100

    referenties op voorpagina

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,7 @@ class HomeController < ApplicationController
 	manageable_content_for :inleiding, :slopen_kort, :verhuur_kort, :titel_zin
 
   def index
+		@reviews = Review.where(:show_on_frontpage => true).first(3)
   end
 
 end
